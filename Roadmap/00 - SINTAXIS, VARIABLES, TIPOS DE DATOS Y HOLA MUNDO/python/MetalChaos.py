42b441d60072c029305377fd9218af1b4d3c7036
MetalChaos-ux <marquezcbg@gmail.com>
2026-04-05 22:13:31 -0400

    MetalChaos.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/MetalChaos.py
@@ -0,0 +1,14 @@
+# https://www.python.org/
+print("https://www.python.org")
+"""
+https://www.python.org
+"""
+Variable_1 = """a"""
+CONSTANTE = """x"""
+
+Touhou = """si"""
+Touhou_principal_favorito = 15
+Touhou_pelea_favorito = 15.5
+touhou_fan = True
+
+print("""¡Hola, Python!""")
